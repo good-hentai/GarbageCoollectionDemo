c662ce643a478bbeb8ba1dd95a3ed96d245e2148
Administrator <Administrator@WYL>
2017-10-02 09:39:55 +0800

    添加项目文件。

---
+++ GarbageCoollectionDemo/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCoollectionDemo
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+
+        public int Divide(int first,int second)
+        {
+            return first / second;
+        }
+
+    }
+}
